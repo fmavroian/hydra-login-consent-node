f47627bb2d34d43745b883647606c76b1b28f28d
Florin Mavroian <florin.mavroian@asurion.com>
2022-01-26 10:11:55 +0900

    moved to .env

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,6 @@ const baseOptions: any = {}
 if (process.env.MOCK_TLS_TERMINATION) {
   baseOptions.headers = { 'X-Forwarded-Proto': 'https' }
 }
-// using Hydra Oauth server to login user
-process.env.HYDRA_ADMIN_URL = "http://127.0.0.1:4445/"
 
 const hydraAdmin = new AdminApi(
   new Configuration({
